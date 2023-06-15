fa36afeb6136d60bd8266ef6e945001b9278ee89
Emre Can Kızılateş <kizilatesemrecan@gmail.com>
2023-06-15 12:04:55 +0300

    updates

---
+++ api/server.js
@@ -0,0 +1,16 @@
+//1. import
+const express = require('express');
+const server = express();
+const usersRouter = require('./Users/users-router');
+
+//2. middleware
+
+//3. router
+server.get('/', (req,res)=>{
+    res.json({message: "Server up and running..."})
+})
+server.use('/api/users', usersRouter)
+//4. error middleware
+
+//export
+module.exports = server;
